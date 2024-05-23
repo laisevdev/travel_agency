c27bb0452c809d857a72319b8d9a83c6de8c6cc6
Laíse Vieira <laisevdev@gmail.com>
2024-05-23 14:22:54 -0300

    [feat]: add admin register

--- agencia/admin.py
+++ agencia/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Clients_Register, Trip_Agenda
 
-# Register your models here.
+admin.site.register(Clients_Register)
+admin.site.register(Trip_Agenda)
