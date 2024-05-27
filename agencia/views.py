10be781cb2912de618d899df973d94defe917f96
Laíse Vieira <laisevdev@gmail.com>
2024-05-26 23:26:07 -0300

    [refactor]: add try and except on client_register and travel_register def

--- agencia/views.py
+++ agencia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm, AgendaForm
 from .models import Clients_Register, Trip_Agenda
+from django.contrib import messages
 
 def index(request):
     return render(request, 'agencia/index.html', {})
@@ -9,12 +10,19 @@ def client_register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.user = request.user
-            post.save()
-            return redirect('sucessfull_register',)
+            try:
+                post = form.save(commit=False)
+                post.user = request.user
+                post.save()
+                messages.success(request, "Registro realizado com sucesso!")
+                return redirect('sucessfull_register')
+            except Exception as e:
+                messages.error(request, f"Ocorreu um erro ao salvar o registro: {e}")
+        else:
+            messages.error(request, "O formulário contém erros. Por favor, corrija-os e tente novamente.")
     else:
         form = RegisterForm()
+    
     return render(request, 'agencia/client_register.html', {'form': form})
 
 def register_sucess(request):
@@ -24,9 +32,14 @@ def travel_register(request):
     if request.method == "POST":
         form = AgendaForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
-            return redirect('sucessfull_booking',)
+            try:
+                post = form.save(commit=False)
+                post.save()
+                return redirect('sucessfull_booking',)
+            except Exception as e:
+                messages.error(request, f"Ocorreu um erro ao salvar o registro da viagem: {e}")
+        else:
+            messages.error(request, "O formulário contém erros. Por favor, corrija-os e tente novamente.")
     else:
         form = AgendaForm()
     return render(request, 'agencia/travel_register.html', {'form': form})     
